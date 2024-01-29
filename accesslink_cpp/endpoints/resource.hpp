9e61ca1a1d3e45475b1a607660f0d5fdf64400b9
andrey <aa.voyko@mail.ru>
2024-01-29 18:24:50 +0300

    add cpr

--- accesslink_cpp/endpoints/resource.hpp
+++ accesslink_cpp/endpoints/resource.hpp
@@ -1,39 +1,41 @@
 #pragma once
 
+#include <cpr/cpr.h>
+
 #include <utility>
+
 #include "../oauth2.hpp"
-#include "unordered_map"
 #include "string"
+#include "unordered_map"
 using Headers = std::unordered_map<std::string, std::string>;
 
 class Resource {
-public:
-    Resource() = default;
-    Resource(const OAuth2Client &oauth) : oauth_(oauth){};
+ public:
+  Resource() = default;
+  Resource(const OAuth2Client &oauth) : oauth_(oauth){};
 
-    template <typename... Args>
-    int _get(Args &&...args) {
-        return oauth_.get(std::forward<Args>(args)...);
-    }
+  template <typename... Args>
+  int _get(Args &&...args) {
+    return oauth_.get(std::forward<Args>(args)...);
+  }
 
-    template <typename... Args>
-    int _post(Args &&...args) {
-        return oauth_.post(std::forward<Args>(args)...);
-    }
+  template <typename... Args>
+  int _post(Args &&...args) {
+    return oauth_.post(std::forward<Args>(args)...);
+  }
 
-    template <typename... Args>
-    int _put(Args &&...args) {
-        return oauth_.put(std::forward<Args>(args)...);
-    }
+  template <typename... Args>
+  int _put(Args &&...args) {
+    return oauth_.put(std::forward<Args>(args)...);
+  }
 
-    template <typename... Args>
-    int _delete(Args &&...args) {
+  template <typename... Args>
+  int _delete(Args &&...args) {
+    return oauth_.remove(std::forward<Args>(args)...);
+  }
 
-        return oauth_.remove(std::forward<Args>(args)...);
-    }
+  ~Resource() = default;
 
-    ~Resource() = default;
-
-protected:
-    OAuth2Client oauth_;
+ protected:
+  OAuth2Client oauth_;
 };
