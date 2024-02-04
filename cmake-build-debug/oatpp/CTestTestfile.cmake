d4d25bb6f9c9e7b42eda23efa8adf6ad959956ea
Nikita Glebskiy <naglebskiy_1@edu.hse.ru>
2024-02-04 17:10:32 +0300

    fix: delete build directory

--- cmake-build-debug/oatpp/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/nikitaglebskij/GarminApp/oatpp
-# Build directory: /Users/nikitaglebskij/GarminApp/cmake-build-debug/oatpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
--- cmake-build-debug/oatpp/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/nikitaglebskij/GarminApp/oatpp/test
-# Build directory: /Users/nikitaglebskij/GarminApp/cmake-build-debug/oatpp/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(oatppAllTests "oatppAllTests")
-set_tests_properties(oatppAllTests PROPERTIES  _BACKTRACE_TRIPLES "/Users/nikitaglebskij/GarminApp/oatpp/test/CMakeLists.txt;135;add_test;/Users/nikitaglebskij/GarminApp/oatpp/test/CMakeLists.txt;0;")
