df58f16351fdc60edbf3b17d2cc7d645c0d49e1c
Nikita Glebskiy <naglebskiy_1@edu.hse.ru>
2024-04-26 15:28:58 +0300

    feat: add polling

--- src/app/driver.h
+++ src/app/driver.h
@@ -10,25 +10,26 @@
 /// SET DEVELOPER_MODE TO 0 WHEN EXPORTING TO DOCKER
 
 namespace Callback {
-constexpr char OAUTHPOINT[] = "/oauth2_callback";
-constexpr char DATAPOINT[] = "/data";
-const int PORT = 5002;
+    constexpr char OAUTHPOINT[] = "/oauth2_callback";
+    constexpr char DATAPOINT[] = "/data";
+    constexpr char WAITPOINT[] = "/wait";
+    const int PORT = 5002;
 }  // namespace Callback
 
 namespace Client {
-const std::string CLIENT_ID = "bd3a77a4-0edc-44e5-81a5-309644f0fc9a";
-const std::string CLIENT_SECRET = "b854a0ca-2a74-4dde-9196-0c0b1b63110e";
+    const std::string CLIENT_ID = "bd3a77a4-0edc-44e5-81a5-309644f0fc9a";
+    const std::string CLIENT_SECRET = "b854a0ca-2a74-4dde-9196-0c0b1b63110e";
 
 #if (DEVELOPER_MODE == 1)
-const std::string REDIRECT_URI = "http://localhost:5002/oauth2_callback";
-const std::string DATA_URI = "http://localhost:5002/data";
+    const std::string REDIRECT_URI = "http://localhost:5002/oauth2_callback";
+    const std::string DATA_URI = "http://localhost:5002/data";
+    const std::string WAIT_URI = "http://localhost:5002/wait";
 #else
-const std::string REDIRECT_URI = "http://172.17.0.2:5002/oauth2_callback";
-const std::string DATA_URI = "http://172.17.0.2:5002/data";
+    const std::string REDIRECT_URI = "http://172.17.0.2:5002/oauth2_callback";
+    const std::string DATA_URI = "http://172.17.0.2:5002/data";
+    const std::string WAIT_URI = "http://172.17.0.2:5002/wait";
 #endif
 };  // namespace Client
-
-using HtmlPage = crow::mustache::rendered_template;
 
 class PolarApp {
 public:
@@ -37,7 +38,9 @@ public:
 
     crow::response Authorize(const crow::request &req);
 
-    HtmlPage ProcessData();
+    crow::response ProcessData();
+
+    crow::response Poll();
 
 private:
     YAML::Node UpdateAccessConfig(const ReadJson &token_response);
