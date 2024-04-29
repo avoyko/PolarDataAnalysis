7c18d553957f6d2e7f12081d5a02661edbbc6c20
Войко Андрей Александрович <aavoyko@edu.hse.ru>
2024-04-29 15:40:15 +0300

    fix: minor fixes

--- src/app/driver.cpp
+++ src/app/driver.cpp
@@ -34,18 +34,21 @@ int PolarApp::Activate() {
     InitialiseMainWindow(screen_config);
 
     CROW_ROUTE(app, "/")
-            ([](const crow::request &req) {
-                CROW_LOG_INFO << "Client is redirected for authorization";
-                crow::response res{200};
-                res.redirect(accesslink.GetAuthUrl());
-                return res;
-            });
+    ([](const crow::request &req) {
+        CROW_LOG_INFO << "Client is redirected for authorization";
+        crow::response res{200};
+        res.redirect(accesslink.GetAuthUrl());
+        return res;
+    });
 
     CROW_ROUTE(app, Callback::OAUTHPOINT)
-            ([this](const crow::request &req) { return Authorize(req); });
+    ([this](const crow::request &req) { return Authorize(req); });
 
     CROW_ROUTE(app, Callback::DATAPOINT)
-            ([this](const crow::request &req) { return ProcessData(); });
+    ([this](const crow::request &req) { return ProcessData(); });
+
+    CROW_ROUTE(app, Callback::WAITPOINT)
+    ([this](const crow::request &req) { return Poll(); });
 
 #if (DEVELOPER_MODE == 1)
     CROW_LOG_INFO << "Navigate to http://localhost:5002/ to register user.";
@@ -53,6 +56,9 @@ int PolarApp::Activate() {
     CROW_LOG_INFO << "Navigate to http://172.17.0.2:5002/ to register user.";
 #endif
     app.port(Callback::PORT).run();
+
+    delwin(screen_config.main_window);
+    endwin();
     return 0;
 }
 
@@ -61,6 +67,7 @@ YAML::Node PolarApp::UpdateAccessConfig(const ReadJson &token_response) {
     YAML::Node config = YAML::LoadFile("../../config.yaml");
     config["user_id"] = std::to_string(token_response["x_user_id"].i());
     config["access_token"] = string_access_token;
+    config["first_run"] = 1;
     std::ofstream file_out("../../config.yaml");
     file_out << config;
     file_out.close();
@@ -94,13 +101,22 @@ crow::response PolarApp::Authorize(const crow::request &req) {
     return res;
 }
 
-crow::mustache::rendered_template PolarApp::ProcessData() {
+crow::response PolarApp::ProcessData() {
     YAML::Node config = YAML::LoadFile("../../config.yaml");
     const auto access_token = config["access_token"].as<std::string>();
     const auto user_id = config["user_id"].as<std::string>();
 
-    InitialiseOptionPanel(screen_config);
-    auto options = ActivateOptionsScreen(screen_config);
+    std::vector<bool> options(2, false);
+    if (config["first_run"].as<int>() == 1) {
+        InitialiseOptionPanel(screen_config);
+        options = ActivateOptionsScreen(screen_config);
+        config["first_run"] = 0;
+        std::ofstream file_out("../../config.yaml");
+        file_out << config;
+        file_out.close();
+    } else {
+        options[0] = true;
+    }
 
     ClearBox(screen_config);
 
@@ -113,7 +129,7 @@ crow::mustache::rendered_template PolarApp::ProcessData() {
     DBWorker &db_worker = DBWorker::GetInstance();
     db_worker.UpdateDB({exercises_info, activity_info, phys_info, sleep_info});
 
-    if (options[1]) {
+    if (options[0]) {
         Model model;
         std::atomic_bool model_finished = false;
 
@@ -134,24 +150,30 @@ crow::mustache::rendered_template PolarApp::ProcessData() {
         CalendarClient calendarClient;
         calendarClient.ScheduleEvents(event_names);
 
-        std::string goodbye_message = "Events for the next are scheduled. Press Enter to exit...";
+        std::string goodbye_message = "Events for the next day are scheduled";
         mvwprintw(screen_config.main_window, screen_config.windowHeight / 4 + 2,
                   (screen_config.windowWidth - goodbye_message.size()) / 2,
                   goodbye_message.c_str());
         wrefresh(screen_config.main_window);
     } else {
-        std::string goodbye_message = "Data is synchronised. Press Enter to exit...";
-        mvwprintw(screen_config.main_window, screen_config.windowHeight / 4,
-                  (screen_config.windowWidth - goodbye_message.size()) / 2,
-                  goodbye_message.c_str());
-        wrefresh(screen_config.main_window);
+        delwin(screen_config.main_window);
+        endwin();
+        exit(0);
     }
 
-    while (wgetch(screen_config.main_window) != 10) {}
-    delwin(screen_config.main_window);
-    endwin();
+    crow::response res{200};
+    res.redirect(Client::WAIT_URI);
+    return res;
+}
 
-    crow::mustache::set_base("../../src/templates");
-    auto page = crow::mustache::load("hello.html");
-    return page.render(user_info);
+crow::response PolarApp::Poll() {
+    int cur_hour = -1;
+    while (cur_hour != 1) { // will upload in 1am
+        auto current_time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+        auto local_time = *std::localtime(&current_time);
+        cur_hour = local_time.tm_hour;
+    }
+    crow::response res{200};
+    res.redirect(Client::DATA_URI);
+    return res; // go to model section
 }
