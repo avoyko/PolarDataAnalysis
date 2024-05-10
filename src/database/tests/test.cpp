cd837f9c6900221ed7a68f58724a153acb01878d
Войко Андрей Александрович <aavoyko@edu.hse.ru>
2024-05-11 00:27:39 +0300

    feat: finish tests

--- src/database/tests/test.cpp
+++ src/database/tests/test.cpp
@@ -4,29 +4,62 @@
 #include "../include/table_phys_info.h"
 #include "../include/table_sleep.h"
 #include "../../model/model_utilities.h"
-
+#include "utilities.h"
 #include <gtest/gtest.h>
 
 TEST(TEST_CREATE_DB, get_instance_method_nothrow) {
     ASSERT_NO_THROW(DBWorker::GetInstance());
 }
 
-TEST(TEST_CREATE_DB, simple_insertion) {
-    DBWorker &db_worker = DBWorker::GetInstance();
-    auto activity_table = db_worker.GetTable("exercise");
-    auto r = activity_table.insert()
-            .values("2030-02-02", "swim", "swim", "swim", "swim", "swim", "swim", "swim",
-                    "swim", "swim", "swim")
-            .execute();
-    ASSERT_NO_THROW(activity_table.select("*").execute());
+TEST(TEST_INSERT_DB, exercise_insertion) {
+    ActivityTable activityTable;
+    ASSERT_NO_THROW(activityTable.InsertIntoTable("2030-02-02", "2329", "428", "250"));
 }
 
-TEST(TEST_CREATE_DB, convert_to_csv) {
+TEST(TEST_INSERT_DB, activity_insertion) {
+    ExercisesTable exercisesTable;
+    ASSERT_NO_THROW(exercisesTable.InsertIntoTable("2030-02-02", "swim", "swim", "swim", "swim",
+                                                   "swim", "swim", "swim", "swim", "swim", "swim"));
+}
+
+TEST(TEST_INSERT_DB, physical_info_insertion) {
+    PhysTable physTable;
+    ASSERT_NO_THROW(
+        physTable.InsertIntoTable("2030-02-02", "80", "180", "160", "60", "123", "123", "12"));
+}
+
+TEST(TEST_INSERT_DB, sleep_insertion) {
+    SleepTable sleepTable;
+    ASSERT_NO_THROW(sleepTable.InsertIntoTable("2030-02-02", "2020-01-01T00:39:07+03:00",
+                                               "2020-01-01T09:19:37+03:00", "1000", "1000", "80"));
+}
+
+TEST(TEST_UPDATE_DB, activity_update) {
+    ActivityTable activityTable;
+    ASSERT_NO_THROW(activityTable.Update(JsonExamples::activity));
+}
+
+TEST(TEST_UPDATE_DB, exercise_update) {
+    ExercisesTable exercisesTable;
+    ASSERT_NO_THROW(exercisesTable.Update(JsonExamples::exercises));
+}
+
+TEST(TEST_UPDATE_DB, phys_info_update) {
+    PhysTable physTable;
+    ASSERT_NO_THROW(physTable.Update(JsonExamples::physical_info));
+}
+
+TEST(TEST_UPDATE_DB, sleep_update) {
+    SleepTable sleepTable;
+    ASSERT_NO_THROW(sleepTable.Update(JsonExamples::sleep));
+}
+
+TEST(TEST_CONVERT_DB, convert_to_csv) {
     DBWorker &db_worker = DBWorker::GetInstance();
     ASSERT_NO_THROW(CSVHelpers::ConvertToCSV());
 }
 
-TEST(TEST_CREATE_DB, delete_csv) {
+TEST(TEST_CONVERT_DB, delete_csv) {
     ASSERT_NO_THROW(CSVHelpers::DeleteCSV());
 }
 
--- src/polar_api/tests/test.cpp
+++ src/polar_api/tests/test.cpp
@@ -21,64 +21,74 @@ inline std::string NormalizedString(const WriteJson& s) {
     return st.substr(1, st.size() - 2);
 }
 
-TEST(TestTransactions, ActivityTransaction) {
-    //    DailyActivity
-    //    accesslink.GetExercises() ASSERT_NO_THROW(1 + 1);
+TEST(TestRetrieveData, Activity) {
+    WriteJson activity_info = accesslink.GetActivity(access_token, user_id)[0];
+    EXPECT_EQ(NormalizedString(activity_info["id"]), "1234");
+    EXPECT_EQ(NormalizedString(activity_info["polar-user"]),
+              "https://www.polaraccesslink/v3/users/1");
+    EXPECT_EQ(NormalizedString(activity_info["transaction-id"]), "179879");
+    EXPECT_EQ(NormalizedString(activity_info["date"]), "2010-12-31");
+    EXPECT_EQ(NormalizedString(activity_info["created"]), "2016-04-27T20:11:33.000Z");
+    EXPECT_EQ(NormalizedString(activity_info["calories"]), "2329");
+    EXPECT_EQ(NormalizedString(activity_info["active-calories"]), "428");
+    EXPECT_EQ(NormalizedString(activity_info["duration"]), "PT2H44M");
+    EXPECT_EQ(NormalizedString(activity_info["active-steps"]), "250");
 }
 
-TEST(TestTransactions, ExerciseTransaction) {
-    ASSERT_NO_THROW(1 + 1);
+TEST(TestRetrieveData, Exercise) {
+    WriteJson exercise = accesslink.GetExercises(access_token, user_id)[0];
+    EXPECT_EQ(NormalizedString(exercise["id"]), "100");
+    EXPECT_EQ(NormalizedString(exercise["upload-time"]), "2008-10-13T10:40:0.000Z");
+    EXPECT_EQ(NormalizedString(exercise["transaction-id"]), "179879");
+    EXPECT_EQ(NormalizedString(exercise["device"]), "Polar M400");
+    EXPECT_EQ(NormalizedString(exercise["device-id"]), "1111AAAA");
+    EXPECT_EQ(NormalizedString(exercise["start-time"]), "2008-10-13T10:40:02");
+    EXPECT_EQ(NormalizedString(exercise["duration"]), "PT2H44M45S");
+    EXPECT_EQ(NormalizedString(exercise["calories"]), "530");
+    EXPECT_EQ(NormalizedString(exercise["distance"]), "1600.2");
 }
-
-TEST(TestTransactions, PhysTransaction) {
-    ASSERT_NO_THROW(1 + 1);
-}
-
- TEST(TestRetrieveData, Activity) {
-     WriteJson activity_info = accesslink.GetActivity(access_token, user_id);
-
- }
-
-//TEST(TestRetrieveData, Exercise) {
-//    WriteJson exercise = accesslink.GetExercises(access_token, user_id)[0];
-//    EXPECT_EQ(NormalizedString(exercise["id"]), "100");
-//    EXPECT_EQ(NormalizedString(exercise["upload-time"]), "2008-10-13T10:40:0.000Z");
-//    EXPECT_EQ(NormalizedString(exercise["transaction-id"]), "179879");
-//    EXPECT_EQ(NormalizedString(exercise["device"]), "Polar M400");
-//    EXPECT_EQ(NormalizedString(exercise["device-id"]), "1111AAAA");
-//    EXPECT_EQ(NormalizedString(exercise["start-time"]), "2008-10-13T10:40:02");
-//    EXPECT_EQ(NormalizedString(exercise["duration"]), "PT2H44M45S");
-//    EXPECT_EQ(NormalizedString(exercise["calories"]), "530");
-//    EXPECT_EQ(NormalizedString(exercise["distance"]), "1600.2");
-//}
-
 
 TEST(TestRetrieveData, PhysicalInfo) {
-    WriteJson exercise = accesslink.GetPhysicalInfo(access_token, user_id)[0];
-    EXPECT_EQ(NormalizedString(exercise["id"]), "123");
-    EXPECT_EQ(NormalizedString(exercise["created"]), "2016-04-27T20:11:33.000Z");
-    EXPECT_EQ(NormalizedString(exercise["polar-user"]),
+    WriteJson physical_info = accesslink.GetPhysicalInfo(access_token, user_id)[0];
+    EXPECT_EQ(NormalizedString(physical_info["id"]), "123");
+    EXPECT_EQ(NormalizedString(physical_info["created"]), "2016-04-27T20:11:33.000Z");
+    EXPECT_EQ(NormalizedString(physical_info["polar-user"]),
               "https://www.polaraccesslink/v3/users/61111666");
-    EXPECT_EQ(NormalizedString(exercise["transaction-id"]), "179879");
-    EXPECT_EQ(NormalizedString(exercise["weight"]), "Polar M400");
-    EXPECT_EQ(NormalizedString(exercise["height"]), "1111AAAA");
-    EXPECT_EQ(NormalizedString(exercise["maximum-heart-rate"]), "2008-10-13T10:40:02");
-    EXPECT_EQ(NormalizedString(exercise["resting-heart-rate"]), "180");
-    EXPECT_EQ(NormalizedString(exercise["aerobic-threshol"]), "PT2H44M45S");
-    EXPECT_EQ(NormalizedString(exercise["anaerobic-threshol"]), "530");
-    EXPECT_EQ(NormalizedString(exercise["vo2-max"]), "1600.2");
-    EXPECT_EQ(NormalizedString(exercise["weight-source"]), "SOURCE_MEASURED");
+    EXPECT_EQ(NormalizedString(physical_info["transaction-id"]), "179879");
+    EXPECT_EQ(NormalizedString(physical_info["weight"]), "80");
+    EXPECT_EQ(NormalizedString(physical_info["height"]), "180");
+    EXPECT_EQ(NormalizedString(physical_info["maximum-heart-rate"]), "160");
+    EXPECT_EQ(NormalizedString(physical_info["resting-heart-rate"]), "60");
+    EXPECT_EQ(NormalizedString(physical_info["aerobic-threshold"]), "123");
+    EXPECT_EQ(NormalizedString(physical_info["anaerobic-threshold"]), "123");
+    EXPECT_EQ(NormalizedString(physical_info["vo2-max"]), "12");
 }
 
- TEST(TestRetrieveData, Sleep) {
-     WriteJson sleep_info = accesslink.GetSleep(access_token, user_id);
-     ASSERT_NO_THROW(1 + 1);
- }
+TEST(TestRetrieveData, Sleep) {
+    WriteJson sleep_info = accesslink.GetSleep(access_token, user_id)[0][0];
 
- TEST(TestRetrieveData, UserInfo) {
-     WriteJson user_info = accesslink.GetUserdata(access_token, user_id);
-     ASSERT_NO_THROW(1 + 1);
- }
+    EXPECT_EQ(NormalizedString(sleep_info["polar_user"]),
+              "https://www.polaraccesslink/v3/users/61111666");
+    EXPECT_EQ(NormalizedString(sleep_info["sleep_start_time"]), "2020-01-01T00:39:07+03:00");
+    EXPECT_EQ(NormalizedString(sleep_info["sleep_end_time"]), "2020-01-01T09:19:37+03:00");
+    EXPECT_EQ(NormalizedString(sleep_info["light_sleep"]), "1000");
+    EXPECT_EQ(NormalizedString(sleep_info["deep_sleep"]), "1000");
+    EXPECT_EQ(NormalizedString(sleep_info["sleep_score"]), "80");
+}
+
+TEST(TestRetrieveData, UserInfo) {
+    WriteJson user_info = accesslink.GetUserdata(access_token, user_id);
+    EXPECT_EQ(NormalizedString(user_info["polar-user-id"]), "2278512");
+    EXPECT_EQ(NormalizedString(user_info["member-id"]), "i09u9ujj");
+    EXPECT_EQ(NormalizedString(user_info["registration-date"]), "2011-10-14T12:50:37.000Z");
+    EXPECT_EQ(NormalizedString(user_info["first-name"]), "Eka");
+    EXPECT_EQ(NormalizedString(user_info["last-name"]), "Toka");
+    EXPECT_EQ(NormalizedString(user_info["birthdate"]), "1985-09-06");
+    EXPECT_EQ(NormalizedString(user_info["gender"]), "MALE");
+    EXPECT_EQ(NormalizedString(user_info["weight"]), "66");
+    EXPECT_EQ(NormalizedString(user_info["height"]), "170");
+    ASSERT_NO_THROW(1 + 1);
+}
 
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
