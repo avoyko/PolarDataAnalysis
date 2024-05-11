d5ae02cf6d15dbc86985481f12f367efcfbe6ec2
Глебский Никита Андреевич <naglebskiy_1@edu.hse.ru>
2024-05-11 13:18:38 +0300

    change scheduling

--- src/calendar_scheduler/scheduler.cpp
+++ src/calendar_scheduler/scheduler.cpp
@@ -4,7 +4,7 @@
 #include "include/scheduler.h"
 #include <unistd.h>
 #include <fcntl.h>
-
+#include <ctime>
 
 #if (DEVELOPER_MODE == 1)
 const std::string venv_executable = "../../venv/bin/python";
@@ -22,15 +22,17 @@ std::string DateStamp::Serialize() const {
 void CalendarClient::PostEvent(const std::string &event_name, DateStamp start_datestamp,
                                DateStamp end_datestamp) {
     std::flush(std::cout);
-    std::string args =
-            "\'" + event_name + "\'" + ' ' + start_datestamp.Serialize() + ' ' + end_datestamp.Serialize();
+    std::string args = "\'" + event_name + "\'" + ' ' + start_datestamp.Serialize() + ' ' +
+                       end_datestamp.Serialize();
     std::string command = venv_executable + " ../calendar_scheduler/post.py " + args;
     std::system(command.c_str());
 }
 
 void CalendarClient::ScheduleEvents(const std::vector<std::string> &event_names) {
+    std::time_t t = std::time(nullptr);
+    std::tm *now = std::localtime(&t);
     for (size_t i = 0; i < event_names.size(); ++i) {
-        DateStamp dateStamp(2024, 04, 26, 18 + i, 00, 00);
+        DateStamp dateStamp(1900 + now->tm_year, 1 + now->tm_mon, now->tm_mday, 18 + i, 00, 00);
         PostEvent(event_names[i], dateStamp, dateStamp);
     }
 }
